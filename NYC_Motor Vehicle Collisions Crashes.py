e47c8ae8c4ff0894f178487f6449aa70ce5611c8
poon6 <100475324+poon6@users.noreply.github.com>
2022-04-09 17:34:37 +0800

    Update NYC_Motor Vehicle Collisions Crashes.py

--- NYC_Motor Vehicle Collisions Crashes.py
+++ NYC_Motor Vehicle Collisions Crashes.py
@@ -6,8 +6,8 @@ import plotly.express as px
 
 #/home/cicada/Downloads/rhyme/streamlit/Motor_Vehicle_Collisions_-_Crashes.csv
 
-DATA_URL = (
-    r"C:\Users\Poon\Downloads\Github upload\streamlit\Motor_Vehicle_Collisions_-_Crashes.csv"
+DATA_URL = ("https://download1527.mediafire.com/9v61rzevmtig/0rtthvazpaioi3h/Motor_Vehicle_Collisions_-_Crashes.csv"
+    #r"C:\Users\Poon\Downloads\Github upload\streamlit\Motor_Vehicle_Collisions_-_Crashes.csv"
 )
 
 st.title("Motor Vehicle Collisions in New York City")
